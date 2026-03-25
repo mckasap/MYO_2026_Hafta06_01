43e28d038ab35c0a39c09b447837e587f0ae2eef
Mustafa Cem Kasapbaşı <mckasap@gmail.com>
2026-03-25 11:30:30 +0300

    dorduncu  Yüklme

--- app/src/main/java/com/example/myo_2026_hafta06_01/MainActivity2.java
+++ app/src/main/java/com/example/myo_2026_hafta06_01/MainActivity2.java
@@ -1,6 +1,15 @@
 package com.example.myo_2026_hafta06_01;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +17,53 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class MainActivity2 extends AppCompatActivity {
 
+    SQLiteDatabase db;
+    ArrayList<Employee> liste= new ArrayList<>();
+    ListView lv;
+    EditText etId,etAd,etSoyad,etEmail;
+
+
+
+    public void dataBaseAC(){
+        db = openOrCreateDatabase("mydb.db", MODE_PRIVATE, null);
+    }
+    public void ListViewGuncelle() {
+        liste.clear();
+
+        db.execSQL("Create Table if not exists " +
+                "Employee( id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, " +
+                "name VARCHAR, " +
+                "surname VARCHAR, " +
+                "email VARCHAR)");
+
+        Cursor c = db.rawQuery("Select * from Employee", null);
+
+        c.moveToFirst();
+        if (c != null) {
+
+            do {
+                int id = c.getInt(0);
+                String name = c.getString(1);
+                String surname = c.getString(2);
+                String email = c.getString(3);
+
+                Log.d("Result", id + " " + name + " " + surname + " " + email);
+                Employee epl = new Employee(id, name, surname, email);
+                liste.add(epl);
+
+            } while (c.moveToNext());
+
+            ArrayAdapter<Employee> adapter = new ArrayAdapter<Employee>(this,
+                    android.R.layout.simple_list_item_1,
+                    liste);
+            lv.setAdapter(adapter);
+
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +74,34 @@ public class MainActivity2 extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        dataBaseAC();
+        lv=(ListView)findViewById(R.id.mylistView);
+        etAd=(EditText)findViewById(R.id.etAd);
+        etSoyad=(EditText)findViewById(R.id.etSoyad);
+        etEmail=(EditText)findViewById(R.id.etEmail);
+        etId=(EditText)findViewById(R.id.etId);
+
+
+            ListViewGuncelle();
+            lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    dataBaseAC();
+                    Employee e=(Employee) parent.getItemAtPosition(position);
+                    Cursor c = db.rawQuery("Select * from Employee where id = " + e.getId() , null);
+                    if(c!=null) {
+                        c.moveToFirst();
+                        etId.setText(""+c.getInt(0));
+                        etAd.setText(""+c.getString(1));
+                        etSoyad.setText(""+c.getString(2));
+                        etEmail.setText(""+c.getString(3));
+                    }
+                    Toast.makeText(getApplicationContext(),"" + e.getId(),Toast.LENGTH_LONG).show();
+                    db.close();
+                }
+            });
+        db.close();
+        }
+
     }
-}
