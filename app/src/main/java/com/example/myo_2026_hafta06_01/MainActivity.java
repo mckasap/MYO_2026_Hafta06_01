24dcf86a53b1c8f6620552a550ec7daaaabc0431
Mustafa Cem Kasapbaşı <mckasap@gmail.com>
2026-03-25 10:40:36 +0300

    Second Yüklme

--- app/src/main/java/com/example/myo_2026_hafta06_01/MainActivity.java
+++ app/src/main/java/com/example/myo_2026_hafta06_01/MainActivity.java
@@ -4,8 +4,11 @@ import android.database.Cursor;
 import android.database.sqlite.*;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -70,7 +73,13 @@ public class MainActivity extends AppCompatActivity {
                     android.R.layout.simple_list_item_1,
                     liste);
             lv.setAdapter(adapter);
-
+ lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+     @Override
+     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+         Employee e=(Employee) parent.getItemAtPosition(position);
+         Toast.makeText(getApplicationContext(),"" + e.getId(),Toast.LENGTH_LONG).show();
+     }
+ });
 
         }
     }
